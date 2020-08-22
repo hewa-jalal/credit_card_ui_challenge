ab3edc7c93f3a0883197f870202d3abc9fa30e02
hiwa-jalal <hiwadoski21@gmail.com>
2020-08-22 10:56:08 +0300

    fixed some mistakes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
-import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
-import 'package:ui_challenge_4/home_page.dart';
+import 'package:ui_challenge_4/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,16 +9,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var controller = MaskedTextController(mask: '00000-00');
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: TextField(
-            controller: controller,
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
